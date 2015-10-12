e2c1c92205e14688024ed44cdf0d0490372bf193
imranwar <imranwar@gmail.com>
2015-10-12 17:19:35 +0500

    v1.0.0

--- SDKs/Aspose.Pdf_Cloud_SDK_for_PHP/src/Aspose/PDF/APIClient.php
+++ SDKs/Aspose.Pdf_Cloud_SDK_for_PHP/src/Aspose/PDF/APIClient.php
@@ -326,7 +326,7 @@ class APIClient {
         return $instance;
     }
 
-    public function isJson($string) {
+    public static function isJson($string) {
         return is_string($string) && is_object(json_decode($string)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
     }
 
