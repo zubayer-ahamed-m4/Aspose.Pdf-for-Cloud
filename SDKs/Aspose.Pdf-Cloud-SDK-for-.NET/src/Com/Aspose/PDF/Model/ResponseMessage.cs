0464ec57e5207f733f3024382674e9095ad8a3ff
Aspose.Slides Product Family <naeem.akram@aspose.com>
2016-10-20 17:29:10 +0500

    Remove LinqBridge, update Aspose.Storage-Cloud reference, fixed all issues on VS 2008, change assembly name to Aspose.Pdf-Cloud and code com

--- SDKs/Aspose.Pdf-Cloud-SDK-for-.NET/src/Com/Aspose/PDF/Model/ResponseMessage.cs
+++ SDKs/Aspose.Pdf-Cloud-SDK-for-.NET/src/Com/Aspose/PDF/Model/ResponseMessage.cs
@@ -7,7 +7,7 @@ namespace Com.Aspose.PDF.Model {
     public class ResponseMessage
     {
         public ResponseMessage() { }
-        public ResponseMessage(byte[] ResponseStream, int Code = 200, String Status = "Ok")
+        public ResponseMessage(byte[] ResponseStream, int Code , String Status)
         {
             this.Code = Code;
             this.Status = Status;
