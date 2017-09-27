a11344e23a47c73f6e4b417f4047fdf4746ffc47
Aspose.Slides Product Family <naeem.akram@aspose.com>
2017-09-27 13:48:00 +0500

    Update Sign Method

--- SDKs/Aspose.Pdf-Cloud-SDK-for-.NET/src/Com/Aspose/PDF/apiInvoker.cs
+++ SDKs/Aspose.Pdf-Cloud-SDK-for-.NET/src/Com/Aspose/PDF/apiInvoker.cs
@@ -105,7 +105,7 @@ namespace Com.Aspose.PDF
 
             // Remove invalid symbols.
             signature = signature.TrimEnd('=');
-            signature = HttpUtility.UrlEncode(signature);
+            //signature = HttpUtility.UrlEncode(signature);
 
             // Convert codes to upper case as they can be updated automatically.
             signature = Regex.Replace(signature, "%[0-9a-f]{2}", e => e.Value.ToUpper());
